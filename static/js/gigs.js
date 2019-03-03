47b7238c3215d67e5bd426bba3fca2e911e758dc
Nacho lopez <nachete-1995@hotmail.com>
2019-03-03 19:15:06 +0100

    added exercise for benny

--- static/js/gigs.js
+++ static/js/gigs.js
@@ -9,27 +9,40 @@ const gigData = {
   // <=
 };
  // PLEASE WHILE ADDING GIGS, WRITTE THEM EXACTLY LIKE THIS:
- // SIMPLY COPY THE FORMAT AND WRITTE YOUR NEXT ONE LIKE 'GIG3'
+
+ // gig4: {
+ //   text: "Thursday February 28th at Fiction Studios, London",
+ //   limit: "28/June/2019"
+ // },
+
+
+ // SIMPLY COPY THE FORMAT AND WRITTE YOUR NEXT ONE LIKE 'GIGX'
  // ADD THE TITLE IN 'TEXT'
  // AND THE DATE OF EXPIRY IN 'LIMIT'
 
  const gigs = {
-    gig1: {
+
+     gig1: {
+       text: "Thursday February 28th at Fiction Studios, London",
+       limit: "28/June/2019"
+     },
+
+    gig2: {
       text: "Thursday March 21st at Fiction Studios, London",
       // THIS WILL BE THE DATE WHEN THE GIG WILL BE REMOVED FROM THE PAGE
       limit: "21/March/2019"
     },
 
-    gig2: {
+    gig3: {
       text: "Thursday April 25th at Fiction Studios, London",
       limit: "25/April/2019",
 
     },
 
-    gig3: {
+    gig4: {
       text: "Thursday May 23rd at Fiction Studios, London",
       limit: "22/May/2019"
-    }
+    },
 
 
 };
