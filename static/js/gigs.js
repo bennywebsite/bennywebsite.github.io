3ee25d54089fba74069aed09ab425930e2b8af88
bennywebsite <47566386+bennywebsite@users.noreply.github.com>
2019-08-06 12:02:17 +0100

    updated bio

--- static/js/gigs.js
+++ static/js/gigs.js
@@ -6,7 +6,7 @@ let gigData = {
 
   bio2: "Benedict released his first EP ‘Piano Tapes’ over the summer of 2018, a completely live performance of 5 tracks recorded at Red Gate Recorders in LA. His first release of 2019 was ‘Therapy,’ a sweeping piano ballad with themes of mental health and self love. This was followed by ‘Fear Of Lonely’, written during Benedict’s time living in Stockholm, Sweden about the loneliness of moving to a new country after a break-up, and 'Funny How Things Change,' which explores the passing of time after a relationship ends and the distance this can create between two people.",
 
-  bio3: "Most recently, Benedict supported Brit Award winner Tom Walker and Grammy nominated Emily King and co-wrote Adam Lambert’s single ‘Feel Something’ which Rolling Stone called “an achingly yearning ballad.” He returns with his fourth single of 2019 'Breaking Hearts,' which focuses on the guilt and regret of breaking the heart of someone you thought you loved.",
+  bio3: "Most recently, Benedict supported Brit Award winner Tom Walker and Grammy nominated Emily King, co-wrote Adam Lambert’s single ‘Feel Something’ which Rolling Stone called “an achingly yearning ballad” and appeared on the BST Hyde Park festival bill supporting Stevie Wonder & Lionel Richie. He returns with 'Breaking Hearts' - his fourth single of 2019 - about the guilt of breaking the hearts of the people you love, and 'Pray No More' - his fifth - which explores issues of identity in religious families.",
   // Video display has a max of 3 links
   video1: "https://www.youtube.com/embed/eEmbj9-BaJk",
 
