d7c9c78f75cbc617c5c3af94687df06a27f35c16
Nacho lopez <nachete-1995@hotmail.com>
2019-12-04 23:22:59 +0100

    changed poster

--- static/js/gigs.js
+++ static/js/gigs.js
@@ -2,11 +2,11 @@
 let gigData = {
   // DO NOT TOUCH THIS =>
   // BIO has up to 3 paragraphs. if empty, it will not be displayed.
-  bio1: "British singer songwriter Benedict Cork has been receiving praise from all over the shop. Elton John: “sensational … a name to look out for,” Billboard : “devastatingly beautiful,” Clash Magazine: “an artist unafraid to let his emotions pour out on a record.”",
+  bio1: "Benedict released his first EP ‘Piano Tapes’ over the summer of 2018, a completely live performance of five tracks recorded at Red Gate Recorders in LA, which garnered support from Elton John who called lead track ‘Ghost’ ‘sensational’ and Benedict ‘a name to look out for.’",
 
-  bio2: "Benedict released his first EP ‘Piano Tapes’ over the summer of 2018, a completely live performance of 5 tracks recorded at Red Gate Recorders in LA. His first release of 2019 was ‘Therapy,’ a sweeping piano ballad with themes of mental health and self love. This was followed by ‘Fear Of Lonely’, written during Benedict’s time living in Stockholm, Sweden about the loneliness of moving to a new country after a break-up, and 'Funny How Things Change,' which explores the passing of time after a relationship ends and the distance this can create between two people.",
+  bio2: "This was followed by his second EP ‘Letters To Strangers’ in August 2019, a collection of six songs recorded between London, LA and Stockholm over the past year and released month by month alongside a residency called ‘Benedict Cork & Friends’ at Fiction Studios.",
 
-  bio3: "Most recently, Benedict supported Brit Award winner Tom Walker and Grammy nominated Emily King, co-wrote Adam Lambert’s single ‘Feel Something’ which Rolling Stone called “an achingly yearning ballad” and appeared on the BST Hyde Park festival bill supporting Stevie Wonder & Lionel Richie. He returns with 'Breaking Hearts' - his fourth single of 2019 - about the guilt of breaking the hearts of the people you love, and 'Pray No More' - his fifth - which explores issues of identity in religious families.",
+  bio3: "Most recently, Benedict has supported Brit Award winner Tom Walker, Grammy nominated Emily King, co-written Adam Lambert’s single ‘Feel Something’ which Rolling Stone called “an achingly yearning ballad” and appeared on the BST Hyde Park festival bill supporting Stevie Wonder & Lionel Richie.",
   // Video display has a max of 3 links
   video1: "https://www.youtube.com/embed/eEmbj9-BaJk",
 
@@ -16,11 +16,11 @@ let gigData = {
 
 
   // picture link of events
-  url: "./static/images/benny_new.jpeg",
-  shop: "https://www.axs.com/uk/events/379339/benedict-cork-tickets?goldenvoiceuk",
+  url: "./static/images/IMG_5660.JPG",
+  shop: "https://www.eventbrite.com/e/benedict-cork-friends-live-at-the-norwegian-church-tickets-79742348511",
   // <=
   // THIS IS THE TITLE OF THE BLOCK =>
-  title: "Duncan Laurence Support Tour"
+  title: "Benedict Cork & Friends - Live at The Norwegian Church"
   // <=
 };
  // PLEASE WHILE ADDING GIGS, WRITTE THEM EXACTLY LIKE THIS:
